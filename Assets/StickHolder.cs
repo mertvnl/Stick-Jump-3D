5a5493c8dbf63d98248d2a0046d1f42df06e9afb
mertvnl <41839620+mertvnl@users.noreply.github.com>
2021-01-01 02:54:24 +0300

    stick anim

---
+++ Assets/StickHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class StickHolder : MonoBehaviour
+{
+    public Transform hand;
+    private void Update()
+    {
+        transform.position = hand.position;
+    }
+}
